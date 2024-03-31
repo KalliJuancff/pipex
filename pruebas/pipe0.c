e8feb6d25105974c6e7978d1b2276ced27134ec2
JC <juancfoofn@yahoo.com>
2024-03-31 21:36:38 +0200

    Add 'pipe0.c', demoing 'pipe' function

---
+++ pruebas/pipe0.c
@@ -0,0 +1,43 @@
+#include <stdio.h>		// perror, printf
+#include <stdlib.h>		// exit
+#include <unistd.h>		// close, fork, pid_t, read, write
+
+#define READ_END 0
+#define WRITE_END 1
+
+int main(void)
+{
+	int fd[2];
+	pid_t pid;
+	char buffer[5];
+	int num_bytes;
+
+	pipe (fd);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error en la ejecución del comando 'fork'.\n");
+		exit(-1);
+	}
+
+	if (pid == 0)
+	{
+		// Proceso hijo
+		close(fd[READ_END]);
+		write(fd[WRITE_END], "gott", 4);
+		close(fd[WRITE_END]);
+		exit(0);
+	}
+	else
+	{
+		// Proceso padre
+		close(fd[WRITE_END]);
+		// El 'read' hace que no necesitemos utilizar la función 'wait'
+		num_bytes = read(fd[READ_END], buffer, sizeof(buffer));
+		buffer[num_bytes] = '\0';	
+		printf("Mensaje escrito por el hijo y leído por el proceso padre: \"%s\" (%d bytes)\n", buffer, num_bytes);
+		close(fd[READ_END]);
+	}
+
+	return (0);
+}
