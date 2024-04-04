30893a0a5215663d65ffedac778b8e20423f50e4
JC <juancfoofn@yahoo.com>
2024-04-04 15:49:08 +0200

    First version that runs, programatically, "ls -l | wc \> 'file'", and works (but requires output file exists)

--- pipex.c
+++ pipex.c
@@ -6,11 +6,14 @@
 /*   By: jfidalgo <jfidalgo@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 10:26:48 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/04/04 12:55:00 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:37:58 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+#define READ_END 0
+#define WRITE_END 1
 
 void	show_program_data(t_prgdata data)
 {
@@ -26,13 +29,63 @@ void	show_program_data(t_prgdata data)
 	printf("Outfile: %s\n", data.outfile);
 }
 
+void	exec_pipex(t_prgdata data)
+{
+	int	fds_pipe[2];
+	int	fd_file;
+	int	pid;
+	int status;
+
+	pipe(fds_pipe);
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error en la ejecución de 'fork'.\n");
+		exit(ERR_EXECUTING_FORK);
+	}
+	if (pid == 0)  // Hijo
+	{
+		close(fds_pipe[READ_END]);
+		dup2(fds_pipe[WRITE_END], STDOUT_FILENO);
+		close(fds_pipe[WRITE_END]);
+		execlp("/bin/ls", "ls", "-l", NULL);
+	}
+	else  // Padre
+	{
+		close(fds_pipe[WRITE_END]);
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Error en la ejecución de 'fork'.\n");
+			exit(ERR_EXECUTING_FORK);
+		}
+		if (pid == 0)  // Hijo2
+		{
+			fd_file = open(data.outfile, O_WRONLY);
+			dup2(fds_pipe[READ_END], STDIN_FILENO);
+			close(fds_pipe[READ_END]);
+			dup2(fd_file, STDOUT_FILENO);
+			execlp("/usr/bin/wc", "wc", NULL);
+		}
+		else  // Padre
+		{
+			close(fds_pipe[READ_END]);
+		}
+	}
+
+	wait(&status);
+	wait(&status);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_prgdata	data;
 
 	validate_arguments(argc, argv);
 	initialize_program_data(&data, argc, argv);
-	show_program_data(data);
+	// show_program_data(data);
+	exec_pipex(data);
 	release_program_data(data);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,24 +6,28 @@
 /*   By: jfidalgo <jfidalgo@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 10:27:15 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/04/04 12:51:35 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:39:29 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
+	// open, OWR_ONLY
+#include <fcntl.h>
 	// perror
 # include <stdio.h>
 	// exit, wait
 # include <stdlib.h>
 	// STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO, ...
-	// ... close, fork, getpid, pid_t, read, write
+	// ... close, dup2, fork, getpid, pid_t, pipe, read, write
 # include <unistd.h>
 
 # include "libft/libft.h"
 
 # define ERR_NUM_PARAMS_KO 1
+# define ERR_EXECUTING_MALLOC 2
+# define ERR_EXECUTING_FORK 3
 
 struct	s_prgdata
 {
